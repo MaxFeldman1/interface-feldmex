c0a5f546d022aad52346a4fc23ef7b968de69d68
jonesliddy77 <jonesliddy77@gmail.com>
2021-02-17 04:54:07 -0600

    about page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,15 +2,53 @@ import React from 'react';
 import '../Home/Home.css';
 import github from '../../assets/github.png';
 import discord from '../../assets/discord.svg';
+import email from '../../assets/email.svg';
+
 
 function Footer() {
 	return (
-		<div className="footer">
-			<b>The Feldmex Variance Swap Platform is unaudited, you are responsible for your funds use at your own risk</b>
-			<a href="https://discord.gg/HrBM8zV" target="_blank" rel="noreferrer"><img alt={"discord.svg"} src={discord} /></a>
-			<a href="https://github.com/Feldmex/varianceSwaps.git" target="_blank" rel="noreferrer"><img alt={"github.png"} src={github} /></a>
-		</div>
-		);
+        <div className="footer">
+            <div className="footer-content">
+                <div className="footer-header">
+                    <h1>Cotact Us</h1>
+                </div>
+                <div className="icon-container">
+                    <a
+                        href="https://discord.gg/HrBM8zV"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            className="icon"
+                            alt={'discord.svg'}
+                            src={discord}
+                        />
+                    </a>
+                    <a
+                        href="https://github.com/Feldmex/varianceSwaps.git"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img className="icon" alt={'github.svg'} src={github} />
+                    </a>
+                    <a
+                        href="mailto:feldmex@protonmail.com"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img className="icon" alt={'email.svg'} src={email} />
+                    </a>
+                </div>
+                <div className="risk">
+					<b>
+                    *The Feldmex Variance Swap Platform is unaudited, you are
+                    responsible for your funds use at your own risk*
+                </b>
+				</div>
+                
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,6 +18,7 @@ function Header() {
 				</div>
 		</div>
 		);
+		
 }
 
 export default Header;
